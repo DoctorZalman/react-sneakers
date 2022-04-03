a7ddaca7c90f77528a60dd3ed2830c1b7cc96281
doctor_zalman <georgesun30@gmail.com>
2022-04-03 20:37:16 +0300

    main container & cart block

---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Drawer.scss';
+
+function Drawer() {
+  return (
+      <div className='overlay'>
+        <div className="drawer">
+        </div>
+      </div>
+  );
+}
+
+export default Drawer;
