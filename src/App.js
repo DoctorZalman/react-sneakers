0e0c2ac46f31f950699f9bb5a2ce260128823dae
doctor_zalman <georgesun30@gmail.com>
2022-04-13 08:27:09 +0300

    add axios, cracte store

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import './styles/styles.css'
 import Header from "./components/Header/Header";
 import MainContainer from "./components/MainContainer/MainContainer";
 import Drawer from "./components/Drawer/Drawer";
+import {useState} from "react";
+import MyContext from "./components/Context/Context";
+
 
 function App() {
+  const [cartOpened, setCartOpened] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+
+  const myContextValue = {cartItems, cartOpened, setCartOpened}
+
+  const onClickCart = () => setCartOpened(true)
+
   return (
-    <div className="wrapper">
-      <Drawer />
-      <Header />
-      <MainContainer />
-    </div>
+      <MyContext.Provider value={myContextValue}>
+        <div className="wrapper">
+          {cartOpened && <Drawer/>}
+          <Header onClickCart={onClickCart}/>
+          <MainContainer addCartItems={setCartItems}/>
+        </div>
+      </MyContext.Provider>
   );
 }
 
--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './Drawer.scss';
-import sneakers from "../../img/sneakers.png";
 import closeButton from "../../img/close-butoon.svg";
+import sneakers from "../../img/sneakers-items/1.png"
+import MyContext from "../Context/Context";
+
 
 function Drawer() {
+
+  const {cartItems, setCartOpened} = useContext(MyContext);
+  const onCloseHandler = (prev) => setCartOpened(!prev)
   return (
       <div className='overlay'>
         <div className="drawer">
           <div className="cart-title">
             <h2>Cart</h2>
-            <img src={closeButton} alt="close-button"/>
+            <img src={closeButton} alt="close-button" onClick={onCloseHandler}/>
           </div>
-          <div className="item">
-            <img src={sneakers} alt="sneakers"/>
-            <div className="title-price">
-              <span>Sneakers Mid Size</span>
-              <p>300$</p>
-            </div>
-            <div className="item-close-button">
-              <img src={closeButton} alt="close-button"/>
-            </div>
+          <div className="items_block">
+            {cartItems.map((obj) => (
+                <div className="item" key={obj.id}>
+                  <img src={sneakers} alt="sneakers"/>
+                  <div className="title-price">
+                    <span>{obj.title}</span>
+                    <p>{obj.price}$</p>
+                  </div>
+                  <div className="item-close-button">
+                    <img src={closeButton} alt="close-button"/>
+                  </div>
+                </div>
+            ))}
           </div>
+
           <div className="item-order">
             <div className="total-price">
               <span>Total:</span>
-              <b>300$</b>
+              <b>{cartItems.price}$</b>
             </div>
             <button>Order</button>
           </div>
